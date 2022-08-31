df2f1ee61187ee823e4e3547cf47408fdb1b0c70
Johannes Ringe <Ringe.Johannes@mh-hannover.de>
2022-08-31 09:42:22 +0200

    init commit

---
+++ MarbleScroll/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarbleScroll
+{
+    static class Program
+    {
+
+        private static MarbleScroll marbleScroll;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            marbleScroll = new MarbleScroll();
+            marbleScroll.FocusWindow = false;
+            marbleScroll.Start();
+            
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.ApplicationExit += new EventHandler(Exit);
+            MarbleForm form = new MarbleForm();
+            Application.Run(); 
+        }
+
+        static void Exit(object sender, EventArgs e)
+        {
+            marbleScroll.Stop();
+        }
+    }
+}
